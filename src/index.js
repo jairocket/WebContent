fed56ee5aa6bb35a6d924ea972d260efc3f879a6
Julia Amorim <bella.juju08@gmail.com>
2024-05-27 14:05:07 +0000

    axios ok

--- src/index.js
+++ src/index.js
@@ -10,25 +10,24 @@ ReactDOM.render(
   document.getElementById('root')
 );
 
+
+
+
 //AXIOS //
 
-// Fazendo uma requisição GET
-axios.get('https:/jsonplaceholder.typicode.com/posts')
-  .then(response => {
-    console.log(response.data);
-  })
-  .catch(error => {
-    console.error('Erro na requisição GET:', error);
-  });
 
-// Fazendo uma requisição POST
-axios.post('https://api.example.com/data', {
-    key1: 'value1',
-    key2: 'value2'
-  })
-  .then(response => {
-    console.log(response.data);
-  })
-  .catch(error => {
-    console.error('Erro na requisição POST:', error);
-  });
+const url = "https://jsonplaceholder.typicode.com/posts"
+
+function getUser() {
+  axios.get(url)
+    .then(response => {
+      console.log(response.data);
+    })
+    .catch(error => {
+      console.error('Erro na requisição GET:', error);
+    });
+}
+
+getUser()
+
+
